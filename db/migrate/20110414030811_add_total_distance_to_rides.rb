18631dfcff0efe81f0922f099d9d53bbf7466f09
jamesbw <james.b.whitbeck@gmail.com>
2011-04-13 23:18:37 -0700

    changed double to float

--- db/migrate/20110414030811_add_total_distance_to_rides.rb
+++ db/migrate/20110414030811_add_total_distance_to_rides.rb
@@ -1,6 +1,6 @@
 class AddTotalDistanceToRides < ActiveRecord::Migration
   def self.up
-    add_column :rides, :total_distance, :double
+    add_column :rides, :total_distance, :float
   end
 
   def self.down
